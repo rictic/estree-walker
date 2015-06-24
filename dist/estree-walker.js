816fc22a194b57e18fd1c3d6c83a5e2e938f7b21
Peter Burns <rictic@google.com>
2015-06-23 18:39:42 -0700

    Checkin dist dir for npm purposes

---
+++ dist/estree-walker.js
@@ -0,0 +1,70 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	factory((global.estreeWalker = {}))
+}(this, function (exports) { 'use strict';
+
+	exports.walk = walk;
+
+	function walk(ast, _ref) {
+		var enter = _ref.enter;
+		var leave = _ref.leave;
+
+		visit(ast, null, enter, leave);
+	}
+
+	var context = {
+		skip: function skip() {
+			return context.shouldSkip = true;
+		}
+	};
+
+	var childKeys = {};
+
+	var toString = Object.prototype.toString;
+
+	function isArray(thing) {
+		return toString.call(thing) === '[object Array]';
+	}
+
+	function visit(node, parent, enter, leave) {
+		if (!node || !node.type) return;
+
+		if (enter) {
+			context.shouldSkip = false;
+			enter.call(context, node, parent);
+			if (context.shouldSkip) return;
+		}
+
+		var keys = childKeys[node.type] || (childKeys[node.type] = Object.keys(node).filter(function (key) {
+			return typeof node[key] === 'object';
+		}));
+
+		var key = undefined,
+		    value = undefined,
+		    i = undefined,
+		    j = undefined;
+
+		i = keys.length;
+		while (i--) {
+			key = keys[i];
+			value = node[key];
+
+			if (isArray(value)) {
+				j = value.length;
+				while (j--) {
+					visit(value[j], node, enter, leave);
+				}
+			} else if (value && value.type) {
+				visit(value, node, enter, leave);
+			}
+		}
+
+		if (leave) {
+			leave(node, parent);
+		}
+	}
+
+}));
+
+//# sourceMappingURL=estree-walker.js.map
